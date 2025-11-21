eccee46f462dfd77d8a31617b23b3af90c5a6cb9
akshaykumar-261 <ak1057357@gmail.com>
2025-11-21 03:53:01 +0000

    conversion opertion in js

---
+++ 01_Basic_javascript/03_conversionOperations.js
@@ -0,0 +1,30 @@
+//let score = "33abc"     //33abc its type is NaN
+//let score = null        
+//let score = undefined    // nan
+//let score = true           // 1
+let score = "Mohan"           //nan
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// conersion of score string into  number
+let valueNumber = Number(score)
+//console.log(typeof valueNumber)
+//console.log(valueNumber)
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Mohan"
+//console.log(isLoggedIn)
+//console.log(typeof (isLoggedIn))
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//console.log(typeof (booleanIsLoggedIn))
+// 1 => true ; 0 => false
+// "" => false
+// "Mohan" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log( someNumber);
+console.log(typeof stringNumber);
