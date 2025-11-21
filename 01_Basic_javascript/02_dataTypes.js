01f22b67e5946d8188346679a737ba332dc0688d
akshaykumar-261 <ak1057357@gmail.com>
2025-11-21 03:15:54 +0000

    DataTypes

---
+++ 01_Basic_javascript/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code in new version
+
+//alert(3 + 3) // we are use with in nodejs , not browser
+
+//Code must be write in readable formate
+console.log("Akshay Kumar");
+console.log(15+23)
+
+//Data types in js
+let name = "Amit"
+let age = 18
+let isLoggedIn = false
+
+// range of In Js number => 2 to power 53
+//bigint
+//string => "Amit" must be declear with in double quotes
+//boolean => true/false
+//null => standalone value
+// undefine => It means abhi tak value nhi deffine ki
+// symbol => uniqe ka liya use hota hai
+
+//object
+console.log(typeof "Amit");
+console.log(typeof age)
+console.log(typeof null) // null ki type object hai *** or null ek object hai
+console.log(typeof undefined) // undefined ki type undefined he hoti hai
