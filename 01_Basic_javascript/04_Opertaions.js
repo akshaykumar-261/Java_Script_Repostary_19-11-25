f5e0bddf712a919847bd0792c68781aef7eff686
akshaykumar-261 <ak1057357@gmail.com>
2025-11-24 04:34:17 +0000

    Operations

---
+++ 01_Basic_javascript/04_Opertaions.js
@@ -0,0 +1,30 @@
+//console.log("helo");
+ //console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+let str1 = "Hello";
+let str2 = " AkshayKumar"
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(+"");
+//******************************************* Comparison in js ********************************
+//console.log("2"<1);
+//console.log("2">1);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+
+console.log(undefined ==0);
+
+console.log(undefined >=0);
+
+console.log(undefined <0);
+
+
